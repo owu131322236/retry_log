ed0aac609c18df6234ed6a71c57cf41fb70f9922
owu131322236 <s1322236@cst.otsuma.ac.jp>
2025-09-18 00:14:03 +0900

    プロフィール画面UIの初期実装

--- src/resources/views/profile/partials/delete-user-form.blade.php
+++ src/resources/views/profile/partials/delete-user-form.blade.php
@@ -1,18 +1,18 @@
 <section class="space-y-6">
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Delete Account') }}
+            {{ __('アカウント削除') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            {{ __('アカウントを削除すると、すべてのデータや設定が完全に消去されます。削除する前に、必要なデータや情報は必ずダウンロードしてください。') }}
         </p>
     </header>
 
     <x-danger-button
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+    >{{ __('アカウントを削除') }}</x-danger-button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
