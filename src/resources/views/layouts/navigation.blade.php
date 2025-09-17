b6b22f1fd70bb7607f9a598fe1f992d42af65d49
owu131322236 <s1322236@cst.otsuma.ac.jp>
2025-09-17 11:03:52 +0900

    homeレイアウトの修正

--- src/resources/views/layouts/navigation.blade.php
+++ src/resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <div class="fixed flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#e7e7f4] bg-[#f8f8fc] w-full h-20 px-10 py-3">
+    <div class="fixed flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#e7e7f4] bg-[#f8f8fc] w-full h-20 z-50 px-10 py-3">
         <div class="flex items-center gap-4 text-[#0d0d1c]">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 16 16"><!-- Icon from Gitlab SVGs by GitLab B.V. - https://gitlab.com/gitlab-org/gitlab-svgs/-/blob/main/LICENSE -->
                 <path fill="#be38f3" fill-rule="evenodd" d="M7.32.029a8 8 0 0 1 7.18 3.307V1.75a.75.75 0 0 1 1.5 0V6h-4.25a.75.75 0 0 1 0-1.5h1.727A6.5 6.5 0 0 0 1.694 6.424A.75.75 0 1 1 .239 6.06A8 8 0 0 1 7.319.03Zm-3.4 14.852A8 8 0 0 0 15.76 9.94a.75.75 0 0 0-1.455-.364A6.5 6.5 0 0 1 2.523 11.5H4.25a.75.75 0 0 0 0-1.5H0v4.25a.75.75 0 0 0 1.5 0v-1.586a8 8 0 0 0 2.42 2.217" clip-rule="evenodd" />
